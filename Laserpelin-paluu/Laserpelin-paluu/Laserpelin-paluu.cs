80ae29e3a3f7a309bfc4bbf31aee007c1295413e
viljasika <viljam.raimi@gmail.com>
2023-06-06 10:34:22 +0300

    Initial commit

---
+++ Laserpelin-paluu/Laserpelin-paluu/Laserpelin-paluu.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace Laserpelin_paluu;
+
+public class Laserpelin_paluu : PhysicsGame
+{
+    public override void Begin()
+    {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
